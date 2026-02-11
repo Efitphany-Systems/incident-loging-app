23a09afb0a237664aef325d7c4467ab17103120b
muhammadazam52564 <muhammadazam52564@gmail.com>
2026-02-11 22:49:50 +0500

    Initial pages

---
+++ src/app/incident-list/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>Incident List Page</div>;
+};
+
+export default page;
---
+++ src/app/incident/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>Incident Page</div>;
+};
+
+export default page;
